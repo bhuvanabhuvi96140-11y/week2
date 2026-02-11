c6d91b433b547c190cb18a6b8fd7448d0582abd6
bhuvanabhuvi96140-11y <bhuvanabhuvi96140@gmail.com>
2026-02-11 21:35:26 +0530

    Delete week2/Day1 directory

--- week2/Day1/Ascending_Descending.py
+++
@@ -1,7 +0,0 @@
-numbers = list(map(int, input("Enter numbers separated by space: ").split()))
-# Ascending Order
-numbers.sort()
-print("Ascending order:", numbers)
-# Descending Order
-numbers.sort(reverse=True)
-print("Descending order:", numbers)
--- week2/Day1/list_to_tuple.py
+++
@@ -1,4 +0,0 @@
-my_list = list(map(int, input("Enter elements separated by space: ").split()))
-my_tuple = tuple(my_list)
-print("List:", my_list)
-print("Tuple:", my_tuple)
