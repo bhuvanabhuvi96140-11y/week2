9bf891914ceb9416221e80498b58a778d74085c5
bhuvanabhuvi96140-11y <bhuvanabhuvi96140@gmail.com>
2026-02-12 18:47:05 +0530

    Add files via upload

---
+++ week2/Day1/list_to_tuple.py
@@ -0,0 +1,4 @@
+my_list = list(map(int, input("Enter elements separated by space: ").split()))
+my_tuple = tuple(my_list)
+print("List:", my_list)
+print("Tuple:", my_tuple)
---
+++ week2/Day2/student_details.py
@@ -0,0 +1,15 @@
+student = {
+    "Name": "Bhuvana",
+    "Age": 22,
+    "Course": "MCA",
+    "College": "UBDT College",
+    "City": "Shivamogga"
+
+}
+print("Student Details:")
+print(student)
+print("\nName:", student["Name"])
+print("\nAge:", student["Age"])
+print("\nCourse:", student["Course"])
+print("\nCollege:", student["College"])
+print("\nCity:", student["City"])
